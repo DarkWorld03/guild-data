f568382d0a2d454bc60324ee78e460f9e2ce7831
DarkWorld03 <jorgeadriel03@gmail.com>
2025-07-23 00:10:46 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde la raíz (index.html, script.js, style.css, y carpetas)
+// Servir archivos estáticos desde la raíz
 app.use(express.static(path.join(__dirname)));
 
-// Ruta raíz
+// Ruta raíz muestra loader.html
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "loader.html"));
+});
+
+// Ruta explícita para index.html
+app.get("/index.html", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
